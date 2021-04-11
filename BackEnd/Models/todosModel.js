160c975f8b688c232a3a4d2ee5c8a98202858dfd
Gyan <gyan@Abhisheks-MacBook-Air-63.local>
2021-04-12 00:10:44 +0530

    API completed

--- BackEnd/Models/todosModel.js
+++ BackEnd/Models/todosModel.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const todosSchema = new Schema({
   user: {type: Schema.Types.ObjectId, ref: 'users'},
-  task: String,
-  completed: Boolean,
-  favorite: Boolean,
-  group: String
+  task: {type: String, required: true},
+  completed: {type: Boolean, default: false},
+  favorite: {type: Boolean, default: false},
+  createdAt: {type: Date, default: Date.now()},
+  cGroup: String
 });
 
 module.exports = mongoose.model("todos", todosSchema);
--- BackEnd/Routes/todosRoutes.js
+++ BackEnd/Routes/todosRoutes.js
@@ -1,51 +1,83 @@
 const router = require("express").Router();
 const Todos = require("../Models/todosModel");
+const Groups = require("../Models/groupModel");
+
 
 // Verify Token
 const verify = require("../verifyToken");
 
 // Get All Todo
 router.get("/", verify, async (req, res) => {
-  const alluser = await Todos.find();
-  res.send({ alluser });
+  const todos = await Todos.find({user: req.user.user._id});
+  res.send({ todos });
 });
 
 // Get Todo by ID
-router.get("/:id", async (req, res) => {
-  const user = await Todos.findById(req.params.id);
-  if (!user) return res.send("Todo Not Found");
-  res.send(user);
-});
-
-// Find all Todos by Id
-router.get("/orders/:id", async (req, res) => {
-  const orders = await USERORDERS.find({ cartId: req.params.id });
-  res.send(orders);
+router.post("/", verify, async (req, res) => {
+  const group = await Groups.findOne({_id: req.body.group});
+  if(!group) return res.send({error: "Invalid group Id"});
+  console.log("===>", req.user);
+  const todo = await new Todos({user: req.user.user._id, task: req.body.task, group: req.body.group}).save();
+  res.send({todo, message: "Successfully created"});
 });
 
 // Update Todo
 router.put("/:id", verify, async (req, res) => {
-  const foundTodo = await Todos.findById(req.params.id);
+  if (req.body.group) {
+    const group = await Groups.findOne({ _id: req.body.group, user: req.user.user._id});
+    if(!group) return re.send({error: "Invalid Group Id"});
+  }
+  const foundTodos = await Todos.findOne({_id: req.params.id});
+  if(!foundTodos) return res.send({error: "Invalid Todos ID"});
   const user = await Todos.updateOne(
     { _id: req.params.id },
     {
       $set: {
-        username: req.body.username ? req.body.username : foundTodo.username,
-        email: req.body.email ? req.body.email : foundTodo.email,
-        role: req.body.role ? req.body.role : foundTodo.role,
-        status: req.body.status ? req.body.status : foundTodo.status,
-        password: req.body.password ? req.body.password : foundTodo.password,
+        task: req.body.task ? req.body.task : foundTodo.task,
+        group: req.body.group ? req.body.group : foundTodo.group
       },
     }
   );
-  res.send({ user });
+  res.send({ message: "Successfully Updated" });
+});
+
+// Update Todo
+router.put("/favorite/:id", verify, async (req, res) => {
+  
+  const foundTodos = await Todos.findOne({_id: req.params.id});
+  if(!foundTodos) return res.send({error: "Invalid Todos ID"});
+  const user = await Todos.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        favorite: !foundTodos.favorite
+      },
+    }
+  );
+  res.send({ message: "Successfully Updated" });
+});
+
+
+// Update Todo
+router.put("/completed/:id", verify, async (req, res) => {
+  const foundTodos = await Todos.findOne({_id: req.params.id});
+  if(!foundTodos) return res.send({error: "Invalid Todos ID"});
+  const user = await Todos.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        completed: !foundTodos.completed 
+      },
+    }
+  );
+  res.send({ message: "Successfully Updated" });
 });
 
 // Delete Todo
 router.delete("/:id", verify, async (req, res) => {
-  const user = await Todos.deleteOne({ _id: req.params.id });
-  if (!user) return res.send("invalid Id");
-  res.send({ user });
+  const todo = await Todos.deleteOne({ _id: req.params.id, user: req.user.user._id });
+  if (!todo) return res.send("invalid Id");
+  res.send({ message: "Successfully deleted" });
 });
 
 module.exports = router;
